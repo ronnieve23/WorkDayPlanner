80680c821f1041f464379bbc5f6c580c52b389ca
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-07-15 15:06:34 -0400

    laid out foundation for code to save tasks

--- assets/script.js
+++ assets/script.js
@@ -7,31 +7,50 @@ $(document).ready(function () {
     $('#currentTime').text(moment().format('h:mm a'));
 
     //For determining if task is past,present, or future and adding colors
-    let currentTime= Number(moment().format("H"))
+    let currentTime = Number(moment().format("H"))
 
     function taskColors() {
-        $(".tasks").each(function(){
+        $(".tasks").each(function () {
             let taskTime = $(this).parent().attr("id")
-                if (taskTime > currentTime){
-                    $(this).addClass("future")
-                } else if (taskTime == currentTime) {
-                    $(this).addClass("present")
-                } else {
-                    $(this).addClass("past")
-                }
+            if (taskTime > currentTime) {
+                $(this).addClass("future")
+            } else if (taskTime == currentTime) {
+                $(this).addClass("present")
+            } else {
+                $(this).addClass("past")
+            }
         });
     }
 
     //check time values to apply approriate color as time passes
-    setInterval(function(){
+    setInterval(function () {
         taskColors()
     }, (1000))
 
     //This where we start saving tasks
 
-    $(".hoursRow").on("click",".saveBtn")
+    $(".hoursRow").on("click", ".saveBtn", function () {
+        let hours = $(this).parent().attr("id")
 
-taskColors();
+        //stores task input
+        let taskInput = $(this).prev(".tasks").val().trim()
+
+        //store values to an object
+        let taskObj = {
+            hour: hours,
+            task: taskInput
+        }
+
+        //index tracking
+            //is the hour block in the array?
+        //if hour block is in array
+            //set index value to current index
+        //if item exists in array
+            //change index value to value of new task
+            //else push ubject to array
+    })
+
+    taskColors();
 })
 
 
