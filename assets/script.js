259d716dfb17acaf8bf6c358ffa8e3f6742108d1
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-07-15 13:49:56 -0400

    Added function to determine if the current hour is past,future, or present
    Hours are color coded depending on past,future,present

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,30 @@ $(document).ready(function () {
     //I hope to god this displays date and time
     $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY, h:mm:ss A'))
 
-    //For determining if task is past,present, or future
-  
-    
+    //For determining if task is past,present, or future and adding colors
+    let currentTime= Number(moment().format("H"))
+
+    function taskColors() {
+        $(".tasks").each(function(){
+            let taskTime = $(this).parent().attr("id")
+                if (taskTime > currentTime){
+                    $(this).addClass("future")
+                } else if (taskTime == currentTime) {
+                    $(this).addClass("present")
+                } else {
+                    $(this).addClass("past")
+                }
+        });
+    }
+
+    //check time values to apply approriate color as time passes
+    setInterval(function(){
+        taskColors()
+    }, (1000*60)*10)
 
 
 
-
-
-
-
-
-    
+taskColors();
 })
 
 
