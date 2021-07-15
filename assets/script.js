95d6eb2e14d092f9fcd807212bd54ab02483cf3b
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-07-15 12:47:49 -0400

    Realized I dont need time-block in html  and removed it

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
     //I hope to god this displays date and time
     $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY, h:mm:ss A'))
 
+    //For determining if task is past,present, or future
     
-}
-)
+})
 
 
